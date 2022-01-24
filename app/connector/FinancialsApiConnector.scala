ffcdd90181af0169589cf159a74fbeee363c229e
DamienButterworth <damienbutterworth@hotmail.co.uk>
2022-01-24 17:26:05 +0000

    MMCA-3357: added TPI01 connectivity

--- app/connector/FinancialsApiConnector.scala
+++ app/connector/FinancialsApiConnector.scala
@@ -16,24 +16,68 @@
 
 package connector
 
+import config.AppConfig
 import models._
+import play.api.libs.json.{Format, Json, OFormat}
 import repositories.ClaimsCache
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
 
 import java.time.LocalDate
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class FinancialsApiConnector @Inject()(claimsCache: ClaimsCache)(implicit executionContext: ExecutionContext) {
+class FinancialsApiConnector @Inject()(httpClient: HttpClient, claimsCache: ClaimsCache, appConfig: AppConfig)(
+  implicit executionContext: ExecutionContext
+) {
 
-  //TODO handle error cases from API (maybe use eithers)
-  def getClaims(eori: String): Future[AllClaims] = {
-    claimsCache.get(eori).flatMap {
-      case Some(value) => Future.successful(value)
-      case None =>
-        simulateRetrieval().flatMap { claims =>
-          claimsCache.set(eori, claims).map { _ => claims }
-        }
-    }.map { claims =>
+  private val baseUrl = appConfig.customsFinancialsApi
+  private val getClaimsUrl = s"$baseUrl/get-claims"
+  private val getSpecificClaimUrl = s"$baseUrl/get-specific-claims"
+
+  case class ClaimsRequest(eori: String)
+
+  object ClaimsRequest {
+    implicit val format: Format[ClaimsRequest] = Json.format[ClaimsRequest]
+  }
+
+  case class SpecificClaimRequest(cdfPayService: String, cdfPayCaseNumber: String)
+
+  object SpecificClaimRequest {
+    implicit val format: Format[SpecificClaimRequest] = Json.format[SpecificClaimRequest]
+  }
+
+  case class CDFPayCaseDetail(CDFPayCaseNumber: String,
+                              CDFPayService: String,
+                              caseStatus: String,
+                              declarantEORI: String,
+                              importerEORI: String,
+                              claimantEORI: Option[String],
+                              claimAmountTotal: Option[String],
+                              totalCaseReimburseAmnt: Option[String]) {
+
+    def toClaim: Claim =
+      caseStatus match {
+        case "Open" => InProgressClaim(CDFPayCaseNumber, LocalDate.of(2000, 1, 1))
+        case "Pending Decision Letter" => PendingClaim(CDFPayCaseNumber, LocalDate.of(2000, 1, 1), LocalDate.of(2000, 1, 1))
+        case "Resolved-Approved" => ClosedClaim(CDFPayCaseNumber, LocalDate.of(2000, 1, 1), LocalDate.of(2000, 2, 1))
+        case _ => throw new RuntimeException("Unhandled error")
+      }
+  }
+
+  object CDFPayCaseDetail {
+    implicit val format: OFormat[CDFPayCaseDetail] = Json.format[CDFPayCaseDetail]
+  }
+
+  case class ClaimsResponse(claims: Seq[CDFPayCaseDetail])
+
+  object ClaimsResponse {
+    implicit val format: OFormat[ClaimsResponse] = Json.format[ClaimsResponse]
+  }
+
+  def getClaims(eori: String)(implicit hc: HeaderCarrier): Future[AllClaims] = {
+    httpClient.POST[ClaimsRequest, ClaimsResponse](getClaimsUrl, ClaimsRequest(eori)).map { response =>
+      val claims: Seq[Claim] = response.claims.map(_.toClaim)
       AllClaims(
         claims.collect { case e: PendingClaim => e },
         claims.collect { case e: InProgressClaim => e },
@@ -55,18 +99,4 @@ class FinancialsApiConnector @Inject()(claimsCache: ClaimsCache)(implicit execut
       "Sarah Philips",
       "sarah.philips@acmecorp.com"
     ))
-
-  def simulateRetrieval(): Future[Seq[Claim]] = {
-    val closedClaims: Seq[ClosedClaim] = (1 to 2).map { value =>
-      ClosedClaim(s"NDRC-${1000 + value}", LocalDate.of(2021, 2, 1).plusDays(value), LocalDate.of(2022, 1, 1).plusDays(value))
-    }
-    val pendingClaims: Seq[PendingClaim] = (1 to 2).map { value =>
-      PendingClaim(s"NDRC-${2000 + value}", LocalDate.of(2021, 2, 1).plusDays(value), LocalDate.of(2022, 1, 1).plusDays(value))
-    }
-    val inProgressClaim: Seq[InProgressClaim] = (1 to 2).map { value =>
-      InProgressClaim(s"NDRC-${3000 + value}", LocalDate.of(2021, 2, 1).plusDays(value))
-    }
-    Future.successful(closedClaims ++ pendingClaims ++ inProgressClaim)
-  }
-
 }
