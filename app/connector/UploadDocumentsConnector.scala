f58535fc276bd1927d3cdcdc5a496f5640c942b8
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-12-09 15:57:44 +0000

    add file upload initialization debug

--- app/connector/UploadDocumentsConnector.scala
+++ app/connector/UploadDocumentsConnector.scala
@@ -29,37 +29,52 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class UploadDocumentsConnector @Inject()(httpClient: HttpClient,
-                                         uploadedFilesCache: UploadedFilesCache
-                                        )(implicit executionContext: ExecutionContext, appConfig: AppConfig) extends Logging {
+class UploadDocumentsConnector @Inject() (httpClient: HttpClient, uploadedFilesCache: UploadedFilesCache)(implicit
+  executionContext: ExecutionContext,
+  appConfig: AppConfig
+) extends Logging {
 
-
-
-  def startFileUpload(caseNumber: String, claimType: ClaimType, serviceType: ServiceType, documentType: FileSelection)(implicit hc: HeaderCarrier, messages: Messages): Future[Option[String]] = {
+  def startFileUpload(caseNumber: String, claimType: ClaimType, serviceType: ServiceType, documentType: FileSelection)(
+    implicit
+    hc: HeaderCarrier,
+    messages: Messages
+  ): Future[Option[String]] = {
     val nonce = Nonce.random
     for {
       previouslyUploaded <- uploadedFilesCache.retrieveCurrentlyUploadedFiles(caseNumber)
-      successfulWrite <- uploadedFilesCache.initializeRecord(caseNumber, nonce, previouslyUploaded)
-      payload = UploadDocumentsWrapper.createPayload(nonce, caseNumber, serviceType, claimType,  documentType, previouslyUploaded)
-      result <- if (successfulWrite) { sendRequest(payload) } else Future.successful(None)
+      successfulWrite    <- uploadedFilesCache.initializeRecord(caseNumber, nonce, previouslyUploaded)
+      payload             =
+        UploadDocumentsWrapper
+          .createPayload(nonce, caseNumber, serviceType, claimType, documentType, previouslyUploaded)
+      result             <- if (successfulWrite) { sendRequest(payload) }
+                            else {
+                              logger.error(
+                                s"Cannot initialize file upload:\nsuccessfulWrite = $successfulWrite\ninitalizationRequest = $payload"
+                              )
+                              Future.successful(None)
+                            }
     } yield result
   }
 
-  def wipeData()(implicit hc: HeaderCarrier): Future[Boolean] = {
-    httpClient.POSTEmpty[HttpResponse](appConfig.fileUploadWipeOutUrl).map(_.status == NO_CONTENT)
-      .recover {
-        case e =>
-          logger.warn(s"Failed to wipe data in upload-document-frontend: $e")
-          false
+  def wipeData()(implicit hc: HeaderCarrier): Future[Boolean] =
+    httpClient
+      .POSTEmpty[HttpResponse](appConfig.fileUploadWipeOutUrl)
+      .map(_.status == NO_CONTENT)
+      .recover { case e =>
+        logger.warn(s"Failed to wipe data in upload-document-frontend: $e")
+        false
       }
-  }
 
-  private def sendRequest(uploadDocumentsWrapper: UploadDocumentsWrapper)(implicit hc: HeaderCarrier): Future[Option[String]] = {
-    httpClient.POST[UploadDocumentsWrapper, HttpResponse](appConfig.fileUploadInitializationUrl, uploadDocumentsWrapper).map { response =>
-      response.status match {
-        case CREATED => response.header("Location")
-        case _ => None
+  private def sendRequest(
+    uploadDocumentsWrapper: UploadDocumentsWrapper
+  )(implicit hc: HeaderCarrier): Future[Option[String]] =
+    httpClient
+      .POST[UploadDocumentsWrapper, HttpResponse](appConfig.fileUploadInitializationUrl, uploadDocumentsWrapper)
+      .map { response =>
+        response.status match {
+          case CREATED => response.header("Location")
+          case _       => None
+        }
       }
-    }.recover { case _ => None }
-  }
+      .recover { case _ => None }
 }
