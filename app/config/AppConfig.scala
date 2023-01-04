ddc641c17d8090eab0ca5cafea7170e6f6805ea2
Robert Udah <airudah2000@gmail.com>
2023-01-04 10:07:19 +0000

    Update URL for guidance page, and year (#83)
    * Update year
    
    * Add URL for guidance page

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -29,6 +29,7 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
   lazy val loginContinueUrl: String         = config.get[String]("urls.loginContinue")
   lazy val homepage: String                 = config.get[String]("urls.homepage")
   lazy val claimServiceUrl: String          = config.get[String]("urls.claimService")
+  lazy val guidancePage: String             = config.get[String]("urls.guidancePage")
   lazy val signOutUrl: String               = config.get[String]("urls.signOut")
   lazy val feedbackService                  = config.getOptional[String]("feedback.url").getOrElse("/feedback") +
     config.getOptional[String]("feedback.source").getOrElse("/CDSRC")
