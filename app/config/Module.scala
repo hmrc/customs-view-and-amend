4d9364bfb03afe69ef602f3732c65137efe6e0d0
Artur Opala <250927+arturopala@users.noreply.github.com>
2023-10-19 16:59:02 +0100

    fix x-correlation-id header structure (#163)
    * fix x-correlation-id header structure
    
    * remove unnecessary config param
    
    * improve outbound requests debugging

--- app/config/Module.scala
+++ app/config/Module.scala
@@ -18,30 +18,27 @@ package config
 
 import akka.actor.ActorSystem
 import com.google.inject.AbstractModule
+import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.{Configuration, Logger}
-import uk.gov.hmrc.auth.core.AuthConnector
 import uk.gov.hmrc.http.hooks.HookData.{FromMap, FromString}
 import uk.gov.hmrc.http.hooks.{Data, HttpHook, RequestData, ResponseData}
-import uk.gov.hmrc.http.{CoreGet, HeaderCarrier, HttpClient}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
 import uk.gov.hmrc.play.audit.http.HttpAuditing
-import uk.gov.hmrc.play.bootstrap.auth.DefaultAuthConnector
 import uk.gov.hmrc.play.bootstrap.http.DefaultHttpClient
 
 import java.net.URL
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.io.AnsiColor._
-import play.api.libs.json.Json
-import scala.util.Success
-import scala.util.Failure
+import scala.util.{Failure, Success, Try}
 
 class Module extends AbstractModule {
+
+  @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
   override def configure(): Unit = {
-    bind(classOf[AuthConnector]).to(classOf[DefaultAuthConnector]).asEagerSingleton()
     bind(classOf[HttpClient]).to(classOf[DebuggingHttpClient])
-    // binding additional interfaces so that libraries that depend on http-verbs can be easily injected
-    bind(classOf[CoreGet]).to(classOf[DebuggingHttpClient])
+    ()
   }
 }
 
@@ -61,6 +58,7 @@ class DebuggingHook(config: Configuration) extends HttpHook {
   val shouldDebug: Boolean =
     config.underlying.getBoolean("outboundRequests.debug")
 
+  @SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
   override def apply(
     verb: String,
     url: URL,
@@ -72,25 +70,20 @@ class DebuggingHook(config: Configuration) extends HttpHook {
   ): Unit = {
     if (shouldDebug && !url.getPath().contains("/auth/authorise")) {
       responseF.andThen {
-        case Success(response)  =>
-          Logger("OutboundRequest").debug(
-            s"""$printRequest  
+        case Success(response) =>
+          Logger("OutboundRequest").debug(s"""$printRequest  
           |$YELLOW Response: $BOLD${response.status}$RESET
           |   ${response.headers.toSeq
-                .flatMap { case (k, vs) => vs.map(v => s"$BLUE$k: $MAGENTA$v$RESET") }
-                .mkString("\n   ")}
+                                              .flatMap { case (k, vs) => vs.map(v => s"$BLUE$k: $MAGENTA$v$RESET") }
+                                              .mkString("\n   ")}
           |      
-          |${response.headers.find(_._1.toLowerCase() == "content-type") match {
-                case Some((key, values)) if values.exists(_.startsWith("application/json")) =>
-                  s"$GREEN${Json.prettyPrint(Json.parse(response.body.value))}$RESET\n"
+          |$GREEN${Try(Json.prettyPrint(Json.parse(response.body.value)))
+                                              .getOrElse(response.body.value)}$RESET\n""".stripMargin)
 
-                case _ => response.body.value
-              }}""".stripMargin
-          )
         case Failure(exception) =>
           Logger("OutboundRequest").debug(
             s"""$printRequest
-            |$RED Failure: $BOLD${exception.toString()}$RESET
+            |$RED_B$WHITE Failure: $BOLD${exception.toString()} $RESET
             |""".stripMargin
           )
       }
@@ -99,22 +92,19 @@ class DebuggingHook(config: Configuration) extends HttpHook {
     def printRequest =
       s"""
         | $BOLD$YELLOW$verb $CYAN$url$RESET 
-        |   ${request.headers.toSeq.map { case (k, v) => s"$BLUE$k: $MAGENTA$v$RESET" }.mkString("\n   ")}
-        |   ${request.body
-          .map { case Data(value, isTruncated, isRedacted) =>
+        |   ${request.headers.map { case (k, v) => s"$BLUE$k: $MAGENTA$v$RESET" }.mkString("\n   ")}
+        |
+        |${request.body
+          .map { case Data(value, _, _) =>
             value match {
-              case FromMap(m)    =>
+              case FromMap(m) =>
                 m.toSeq
                   .flatMap { case (k, vs) => vs.map(v => (k, v)) }
                   .map { case (k, v) => s"$k = $v" }
                   .mkString("\n   ")
-              case FromString(s) =>
-                request.headers.find(_._1.toLowerCase() == "content-type") match {
-                  case Some(value) if value.toString().startsWith("application/json") =>
-                    s"$GREEN${Json.prettyPrint(Json.parse(s))}$RESET"
 
-                  case _ => s
-                }
+              case FromString(s) =>
+                s"$GREEN${Try(Json.prettyPrint(Json.parse(s))).getOrElse(s)}$RESET"
             }
           }
           .getOrElse("")}""".stripMargin
