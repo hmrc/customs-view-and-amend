6338af3e2f5262179a6a2ecd5e72495ec4383229
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-04-01 20:35:53 +0100

    [TechDebt] Spring'25 dependencies upgrade (#211)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapFrontendPlayVersion = "9.6.0"
-  val hmrcMongoPlayVersion         = "2.4.0"
+  val bootstrapFrontendPlayVersion = "9.11.0"
+  val hmrcMongoPlayVersion         = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapFrontendPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "org.typelevel"     %% "cats-core"                  % "2.12.0",
-    "org.jsoup"          % "jsoup"                      % "1.18.2"
+    "org.typelevel"     %% "cats-core"                  % "2.13.0",
+    "org.jsoup"          % "jsoup"                      % "1.19.1"
   )
 
   val test = Seq(
