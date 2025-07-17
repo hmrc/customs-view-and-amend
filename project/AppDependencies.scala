ecb4917fbd7975aab5a3782f4768d2bd9085ae2d
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-07-17 11:03:19 +0200

    upgrade depoendencies and apply platops-pr-bot comments (#226)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapFrontendPlayVersion = "9.11.0"
+  val bootstrapFrontendPlayVersion = "9.16.0"
   val hmrcMongoPlayVersion         = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapFrontendPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.7.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"     %% "cats-core"                  % "2.13.0",
     "org.jsoup"          % "jsoup"                      % "1.19.1"
