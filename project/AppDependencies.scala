42ea8595741b8b059be031b60bb96d8216d61d2d
sergi-hmrc <97970044+sergi-hmrc@users.noreply.github.com>
2026-08-28 13:20:51 +0100

    DCWL-4864: add customs-view-and-amend-acceptance-tests to repository.… (#244)
    * DCWL-4864: add customs-view-and-amend-acceptance-tests to repository.yaml
    
    * DCWL-4864: uptick libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapFrontendPlayVersion = "10.7.0"
-  val hmrcMongoPlayVersion         = "2.12.0"
+  val hmrcMongoPlayVersion         = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapFrontendPlayVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "13.11.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"     %% "cats-core"                  % "2.13.0",
-    "org.jsoup"          % "jsoup"                      % "1.22.2"
+    "org.jsoup"          % "jsoup"                      % "1.23.2"
   )
 
   val test: Seq[ModuleID] = Seq(
